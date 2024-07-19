c3bbbd7f4f79a7ca498e00a36e24e043a2506ef3
balbarka <balbarka>
2024-07-19 15:08:33 +0000

    modify model.py to load in deploy context

--- databricks/app/01_Foundation_LangChain_RAG/model.py
+++ databricks/app/01_Foundation_LangChain_RAG/model.py
@@ -7,57 +7,72 @@ from langchain_community.vectorstores import DatabricksVectorSearch
 from langchain.schema.runnable import RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.documents.base import Document
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.chat_models import ChatDatabricks
-from operator import itemgetter
-
 
 ## Enable MLflow Tracing
 mlflow.langchain.autolog()
 
 model_config = mlflow.models.ModelConfig(development_config="config.yaml")
 
-@RunnableLambda
-def last_user_content(messages: [dict]):
-    return messages[-1]["content"]
+# Connect to the Vector Search Index
+vs_index = VectorSearchClient(disable_notice=True).get_index(
+    endpoint_name=model_config.get("vector_search_endpoint_name"),
+    index_name=model_config.get("vector_search_index_name"),
+)
 
+# Turn the Vector Search index into a LangChain retriever
+vector_search_as_retriever = DatabricksVectorSearch(
+    vs_index,
+    text_column="content",
+    columns=["id", "content"],
+).as_retriever(search_kwargs={"k": 3}) # Number of search results that the retriever returns
+# Enable the RAG Studio Review App and MLFlow to properly display track and display retrieved chunks for evaluation
+mlflow.models.set_retriever_schema(primary_key="id", text_column="content")
 
-def get_lc_retriever(endpoint_name: str, index_name: str, k=3):
-    return DatabricksVectorSearch(index=VectorSearchClient(disable_notice=True).get_index(endpoint_name, index_name),
-                                  text_column="content",
-                                  columns=["id", "content"]).as_retriever(search_kwargs={"k": k})
+# Method to format the docs returned by the retriever into the prompt (keep only the text from chunks)
+def format_context(docs):
+    chunk_contents = [f"Passage: {d.page_content}\n" for d in docs]
+    return "".join(chunk_contents)
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatDatabricks
+from operator import itemgetter
 
-@RunnableLambda
-def format_search_results(docs: [Document]):
-    return "".join([f"Passage: {d.page_content}\n" for d in docs])
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (  # System prompt contains the instructions
+            "system",
+            """You are an assistant that answers questions. Use the following pieces of retrieved context to answer the question. Some pieces of context may be irrelevant, in which case you should not use them to form the answer.
 
+Context: {context}""",
+        ),
+        # User's question
+        ("user", "{question}"),
+    ]
+)
 
-def get_lc_prompt_template(llm_prompt_template: str) -> ChatPromptTemplate:
-    return ChatPromptTemplate.from_messages([("system", llm_prompt_template),
-                                             ("user", "{question}")])
+# Our foundation model answering the final prompt
+model = ChatDatabricks(
+    endpoint=model_config.get("llm_model_serving_endpoint_name"),
+    extra_params={"temperature": 0.01, "max_tokens": 500}
+)
 
+# Return the string contents of the most recent messages: [{...}] from the user to be used as input question
+def extract_user_query_string(chat_messages_array):
+    return chat_messages_array[-1]["content"]
 
-def get_lc_model(endpoint_name: str) -> ChatDatabricks:
-    return ChatDatabricks(endpoint=endpoint_name,
-                          extra_params={"temperature": 0.01, "max_tokens": 500})
-
-
-def get_lc_chain(endpoint_name: str,
-                 index_name: str,
-                 prompt_template: str,
-                 model_name: str):
-    lc_retriever = get_lc_retriever(endpoint_name, index_name)
-    lc_prompt = get_lc_prompt_template(prompt_template)
-    lc_model = get_lc_model(model_name)
-    return ({"question": itemgetter("messages") | last_user_content,
-             "context": itemgetter("messages") | last_user_content | lc_retriever | format_search_results}
-            | lc_prompt | lc_model | StrOutputParser())
-
-chain = get_lc_chain(endpoint_name=model_config.get('vector_search_endpoint_name'),
-                     index_name=model_config.get('vector_search_index_name'),
-                     prompt_template=model_config.get('llm_prompt_template'),
-                     model_name=model_config.get('llm_model_serving_endpoint_name'))
+# RAG Chain
+chain = (
+    {
+        "question": itemgetter("messages") | RunnableLambda(extract_user_query_string),
+        "context": itemgetter("messages")
+        | RunnableLambda(extract_user_query_string)
+        | vector_search_as_retriever
+        | RunnableLambda(format_context),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
+)
 
 # Tell MLflow logging where to find your chain.
 mlflow.models.set_model(model=chain)
